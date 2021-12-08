74b5820e347208c460f05fd652593e0174d9731a
HelgeJohannsen <hj@commerce-pro.de>
2021-12-08 06:01:11 +0100

    checkoutcreate Server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ import Shopify, { ApiVersion } from "@shopify/shopify-api";
 import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
-import bodyParser from "koa-bodyparser";
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
