354eaf74961c448df49a524d74bf9bda06078b4f
HelgeJohannsen <hj@commerce-pro.de>
2021-12-08 05:05:35 +0100

    step1 tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
+import React from 'react';
 import { Heading, Page, TextStyle, Layout, EmptyState} from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
+// Sets the state for the resource picker
 class Index extends React.Component {
   state = { open: false };
   render() {
     return (
       <Page>
+        <TitleBar
+          primaryAction={{
+            content: 'Select products',
+            onAction: () => this.setState({ open: true }),
+          }}
+        />
+        <ResourcePicker // Resource picker component
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+        />
         <Layout>
-          <EmptyState // Empty state component
+          <EmptyState
             heading="Discount your products temporarily"
             action={{
               content: 'Select products',
@@ -20,8 +36,12 @@ class Index extends React.Component {
           </EmptyState>
         </Layout>
       </Page>
-    )
+    );
   }
-};
+  handleSelection = (resources) => {
+    this.setState({ open: false });
+    console.log(resources);
+  };
+}
 
 export default Index;
