f3fddbcd4555fda34f37567c467dc440a211db19
abv313 <helge.johannsen@haw-hamburg.de>
2021-12-18 12:47:56 +0100

    new webhook

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
-  API_VERSION: ApiVersion.January21,
+  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October21,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
@@ -48,6 +48,7 @@ app.prepare().then(async () => {
             productId: undefined,
           },
         };
+
         const response = await Shopify.Webhooks.Registry.register({
           shop,
           accessToken,
@@ -55,35 +56,18 @@ app.prepare().then(async () => {
           topic: "APP_UNINSTALLED",
           webhookHandler: async (_, shop) => delete ACTIVE_SHOPIFY_SHOPS[shop],
         });
+
         if (!response.success) {
           console.log(
             `Failed to register APP_UNINSTALLED webhook: ${response.result}`
           );
         }
-        /* Register webhook for cart update*/
-        const registrationCartUpdate = await Shopify.Webhooks.Registry.register({
-          shop,
-          accessToken,
-          path: '/webhooks',
-          topic: 'CARTS_UPDATE',
-          webhookHandler: async (_topic, shop, body) => {
-            console.log('received cart update webhook: ');
-            const obj = JSON.parse(body);
-            console.log(obj);
-          },
-        });
-        if (registrationCartUpdate.success) {
-          console.log('Successfully registered cart update webhook!');
-        } else {
-          console.log('Failed to register cart update webhook', registrationCartUpdate.result);
-        }
+
         // Redirect to app with shop parameter upon auth
         ctx.redirect(`/?shop=${shop}&host=${host}`);
       },
     })
   );
-
-
 
   const handleRequest = async (ctx) => {
     await handle(ctx.req, ctx.res);
@@ -92,21 +76,12 @@ app.prepare().then(async () => {
   };
 
   router.post("/webhooks", async (ctx) => {
-    const client = new Shopify.Clients.Rest('helgedev.myshopify.com', accessToken);
-    const data = await client.post({
-      path: 'webhooks',
-      data: {"webhook":{"topic":"orders\/create","address":"https:\/\/https://globusapp.herokuapp.com\/","format":"json","fields":["id","note"]}},
-      type: DataType.JSON,
-    });
-    console.log("data");
-    console.log(data);
     try {
       await Shopify.Webhooks.Registry.process(ctx.req, ctx.res);
       console.log(`Webhook processed, returned status code 200`);
     } catch (error) {
       console.log(`Failed to process webhook: ${error}`);
     }
-
   });
 
   router.get("(/_next/static/.*)", handleRequest); // Static content is clear
@@ -147,7 +122,6 @@ app.prepare().then(async () => {
     ctx.status = 200;
   });
 
-
   router.post("/settings", async (ctx) => {
     const session = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res);
     const shop = session.shop;
@@ -160,8 +134,7 @@ app.prepare().then(async () => {
 
     const productIdStruct = JSON.parse(ctx.request.body).productId.split("/");
     const productId = productIdStruct[productIdStruct.length - 1];
-    console.log("ctx.query");
-    console.log(ctx.query);
+
     ACTIVE_SHOPIFY_SHOPS[shop].settings = { productId };
 
     const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
